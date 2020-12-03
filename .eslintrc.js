a21ae1a0d431fb3e065a7213efab826217164e6e
Minh NH <minhnh@edoctor.vn>
2020-12-03 17:14:38 +0700

    update module

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  extends: 'edoctor-react-app',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  settings: { 'import/resolver': { typescript: {} } },
-};
